3c4fc7870fe1d1f92a1dd69d7e5d4d2cbbd4c358
unknown <darrenheath23@hotmail.com>
2022-09-19 10:31:02 +0200

    Solved lab

--- js/index.js
+++ js/index.js
@@ -1,30 +1,38 @@
 // ITERATION 1
+const buttonRemove = document.querySelectorAll(".btn-remove")
+const table = document.querySelector("tbody")
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+function updateSubtotal() {
+  const price = product.querySelector(".price span")
+  const quantity =  product.querySelector(".quantity input")
+  const subTotalElement = product.querySelector(".subtotal span")
+  subTotalElement.innerHTML = price.innerHTML * quantity.value
+return subTotalElement.innerHTML
 }
 
 function calculateAll() {
+  const totalValue = document.querySelector(".total-value span")
+  const totalSum = updateSubtotal()
+  totalValue.innerHTML = totalSum
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
 
+    //console.log(allProducts);
+  }
   // ITERATION 3
   //... your code goes here
-}
+
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+    const currentElement = event.target.parentNode.parentNode;
+    table.removeChild(currentElement)
   //... your code goes here
 }
 
@@ -40,3 +48,9 @@ window.addEventListener('load', () => {
 
   //... your code goes here
 });
+
+[...buttonRemove].map((button) => {
+  button.addEventListener('click', function(event){
+    removeProduct(event);
+  });
+}) 
